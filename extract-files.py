Linux-On-LineageOS/android_device_google_flame

Update extract-files helper scripts
Change-Id: I42081d2ac09e739ecb12548f05c096d1a72b7252

extract-files.py
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+pushd "${MY_DIR}/../coral/flame"
+./extract-files.py $@
+popd

setup-makefiles.py
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+pushd "${MY_DIR}/../coral/flame"
+./setup-makefiles.py
+popd

Evolution-X-Devices/device_google_blueline

extract-files.py
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+pushd "${MY_DIR}/../crosshatch/blueline"
+./extract-files.py $@
+popd

setup-makefiles.py
@@ -1,0 +1,13 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+MY_DIR="$(cd "$(dirname "${0}")"; pwd -P)"
+
+pushd "${MY_DIR}/../crosshatch/blueline"
+./setup-makefiles.py
+popd